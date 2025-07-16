4d73bc44bdca6eaafafa41d677c5f5c67b1ba2da
Juul-Mc-Goa <jules.magois.pro@proton.me>
2025-07-16 09:45:50 +0200

    add `aes, ctr, sha3` dependencies, more constants

--- src/constants.rs
+++ src/constants.rs
@@ -1,10 +1,14 @@
-/// the prime number `q` used throughout
+/// the prime number `p` used throughout
 pub const PRIME: u64 = (1 << 40) - 195;
 #[allow(dead_code)]
 pub const LOG_PRIME: u64 = 40;
 
 /// the degree `d` defining `R_q = (Z/qZ)[X] / (X^d + 1)`
 pub const DEGREE: u64 = 64;
+
+pub const JL_MAX_NORM: u64 = 1 << 28;
+pub const JL_MAX_NORM_SQ: u64 = 1 << 56;
+pub const AES128_BLOCK_BYTES: u64 = 512;
 
 lazy_static! {
     /// the slack factor introduced when proving norm bounds
