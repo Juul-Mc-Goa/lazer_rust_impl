51c50d2d6d86fe66bddfd758c03d7e25e54bbfba
Juul-Mc-Goa <jules.magois.pro@proton.me>
2025-07-16 17:42:42 +0200

    main: test aes128 CTR mode

--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,22 @@ mod ring;
 mod witness;
 
 fn main() {
-    println!("Hello, world!");
+    use aes::cipher::{KeyIvInit, StreamCipher};
+    use witness::Aes128Ctr64LE;
+
+    let hashbuf = [0u8; 16];
+    let mut cipher = Aes128Ctr64LE::new(&hashbuf.into(), &0_u128.to_le_bytes().into());
+
+    for _ in 0..8 {
+        let mut buf = vec![0_u8; 512];
+        cipher.apply_keystream(&mut buf);
+
+        for i in 0..32 {
+            for j in 0..16 {
+                print!("{:3} ", buf[16 * i + j])
+            }
+            print!("\n");
+        }
+        print!("\n");
+    }
 }
