717937d43e2e23286b28483ded799ce9b57de138
Juul-Mc-Goa <jules.magois.pro@proton.me>
2025-07-18 13:21:23 +0200

    define sparse matrix

--- src/matrices.rs
+++ src/matrices.rs
@@ -4,6 +4,10 @@ pub struct PolyMatrix(pub Vec<Vec<PolyRingElem>>);
 #[allow(dead_code)]
 pub struct BaseMatrix(pub Vec<Vec<BaseRingElem>>);
 
+/// Sparse matrix with polynomial coefficients: a vector of
+/// `(row_index, column_index, coef)`.
+pub struct SparsePolyMatrix(pub Vec<(usize, usize, PolyRingElem)>);
+
 impl From<PolyMatrix> for BaseMatrix {
     fn from(_value: PolyMatrix) -> Self {
         todo!()
