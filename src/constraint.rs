9506d0b326cffdefc7b1e2067bc0ec93489c29ea
Juul-Mc-Goa <jules.magois.pro@proton.me>
2025-07-23 15:50:21 +0200

    work on jl_proj_collapsmat

--- src/constraint.rs
+++ src/constraint.rs
@@ -1,4 +1,13 @@
-use crate::{matrices::SparsePolyMatrix, ring::PolyRingElem};
+use crate::{
+    constants::{LOG_PRIME, PRIME_BYTES_LEN},
+    linear_algebra::{PolyVec, SparsePolyMatrix},
+    ring::{BaseRingElem, PolyRingElem},
+};
+
+use sha3::{
+    Shake128,
+    digest::{ExtendableOutput, Update, XofReader},
+};
 
 /// A degree 2 polynomial in the `r` witness vectors s_1, ..., s_r in R_q^n:
 ///
@@ -12,7 +21,7 @@ use crate::{matrices::SparsePolyMatrix, ring::PolyRingElem};
 pub struct Constraint {
     pub degree: usize,
     pub quadratic_part: SparsePolyMatrix,
-    pub linear_part: Vec<PolyRingElem>,
+    pub linear_part: PolyVec,
     pub constant: PolyRingElem,
 }
 
@@ -21,11 +30,57 @@ impl Constraint {
         Self {
             degree,
             quadratic_part: SparsePolyMatrix(Vec::new()),
-            linear_part: Vec::new(),
+            linear_part: PolyVec::new(),
             constant: PolyRingElem::zero(),
         }
     }
     pub fn new() -> Self {
         Self::new_raw(1)
     }
+}
+
+#[allow(dead_code)]
+pub fn collaps_jl_proj_raw(
+    constraint: &mut Constraint,
+    r: usize,
+    dim: usize,
+    hash: &mut [u8; 16],
+    projection: &[BaseRingElem; 256],
+    jl_matrix: &Vec<u8>,
+) {
+    let mut hasher = Shake128::default();
+    hasher.update(hash);
+    let mut reader = hasher.finalize_xof();
+
+    // REVIEW: hashbuf length
+    // hashbuf: 32 bytes, then challenges (256 BaseRingElem), then ???
+    let mut hashbuf = [0_u8; 32 + PRIME_BYTES_LEN * 256 + 24];
+    reader.read(&mut hashbuf);
+    // update hash
+    hash.copy_from_slice(&hashbuf[..16]);
+
+    let challenges = &hashbuf[32..];
+    let mut alpha = [0_u64; 256];
+
+    let build_u64 = |limbs: &[u8]| {
+        let mut shift = 1 << 8_u64;
+        let mut result = limbs[0] as u64;
+        for i in 1..PRIME_BYTES_LEN {
+            result += shift * limbs[i] as u64;
+            shift *= 256;
+        }
+
+        result
+    };
+
+    // 256 = 32 * 8
+    let primes_len = LOG_PRIME as usize;
+    for i in 0..32 {
+        for j in 0..8 {
+            alpha[8 * i + j] = build_u64(&challenges[(primes_len * i + PRIME_BYTES_LEN * j)..]);
+        }
+    }
+
+    // TODO: polxvec_jlproj_collapsmat: build constraint.linear_part from jl_matrix and hashbuf
+    // TODO: jlproj_collapsproj: build constraint.constant from challenges ?
 }
