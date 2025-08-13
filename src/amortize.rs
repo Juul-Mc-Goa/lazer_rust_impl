926eddf209f4060c4216b6da5089cbb97347584b
Juul-Mc-Goa <jules.magois.pro@proton.me>
2025-08-13 15:04:46 +0200

    amortize: work on the no_tail variant

--- src/amortize.rs
+++ src/amortize.rs
@@ -1,4 +1,18 @@
-use crate::{commit::CommitParams, proof::Proof, statement::Statement, witness::Witness};
+use crate::{
+    commit::{CommitKey, CommitKeyData, CommitParams, Commitments},
+    linear_algebra::PolyVec,
+    proof::Proof,
+    ring::PolyRingElem,
+    statement::Statement,
+    witness::Witness,
+};
+
+use rand::SeedableRng;
+use rand_chacha::ChaCha8Rng;
+use sha3::{
+    Shake128,
+    digest::{ExtendableOutput, Update, XofReader},
+};
 
 pub fn amortize_tail(
     output_stat: &mut Statement,
@@ -24,12 +38,12 @@ pub fn amortize(
     output_wit: &mut Witness,
     proof: &mut Proof,
     tmp_wit: &Witness,
+    commit_key: &CommitKey,
 ) {
     if proof.tail {
         amortize_tail(output_stat, output_wit, proof, tmp_wit);
     }
 
-    let com_params = output_stat.commit_params;
     let Statement {
         r,
         dim,
@@ -41,32 +55,105 @@ pub fn amortize(
                 z_length,
                 uniform_base: unif_base,
                 uniform_length: unif_len,
-                quadratic_base: quad_base,
-                quadratic_length: quad_len,
-                commit_rank_1: com_rank_1,
+                quadratic_base: _,
+                quadratic_length: _,
+                commit_rank_1: _,
                 commit_rank_2: com_rank_2,
                 ..
             },
-        commitments,
+        commitments:
+            Commitments::NoTail {
+                inner: _,
+                u1: _,
+                u2,
+            },
         challenges,
         constraint,
         squared_norm_bound,
         hash,
-    } = output_stat;
-    let (r, unif_base, unif_len, quad_base, quad_len, com_rank_1, com_rank_2) = (
-        *r,
-        *unif_base,
-        *unif_len,
-        *quad_base,
-        *quad_len,
-        *com_rank_1,
-        *com_rank_2,
-    );
+    } = output_stat
+    else {
+        panic!("amortize: output statement is `Tail`");
+    };
+    let (dim, r, z_length, unif_base, unif_len, com_rank_2) =
+        (*dim, *r, *z_length, *unif_base, *unif_len, *com_rank_2);
 
-    let h_start = r + unif_len * com_rank_1 + quad_len * r * (r + 1) / 2;
-    let (_, wit_h) = output_wit.vectors[com_params.z_length]
-        .0
-        .split_at_mut(h_start);
+    // compute linear garbage
+    let linear_part = &constraint.linear_part;
+    let dim_h = unif_len * r * (r + 1) / 2;
+    let mut h: PolyVec = PolyVec(Vec::with_capacity(dim_h));
 
-    todo!()
+    for k in 0..dim {
+        for i in 0..r {
+            let coef_ik = &linear_part[i].0[k];
+            let wit_ik = &tmp_wit.vectors[i].0[k];
+            h.0.push(coef_ik * wit_ik);
+            for j in i..r {
+                let coef_jk = &linear_part[j].0[k];
+                let wit_jk = &tmp_wit.vectors[j].0[k];
+                h.0.push(coef_ik * wit_jk + coef_jk * wit_ik);
+            }
+        }
+    }
+
+    // decompose linear garbage
+    let mut h = h.decomp(unif_base);
+
+    let CommitKey {
+        data:
+            CommitKeyData::NoTail {
+                matrix_a: _,
+                matrices_b: _,
+                matrices_c: _,
+                matrices_d,
+            },
+        seed: _,
+    } = commit_key
+    else {
+        panic!()
+    };
+
+    // compute u2
+    *u2 = PolyVec::zero(com_rank_2);
+    for (h_digit, mat_level) in h.iter().zip(matrices_d.iter()) {
+        for i in 0..r {
+            mat_level[i].add_apply_raw(&mut u2.0, &h_digit.0[i..]);
+        }
+    }
+
+    // use u2 to update output_stat.hash
+    // initialise hasher
+    let mut hasher = Shake128::default();
+    hasher.update(hash);
+    hasher.update(&u2.iter_bytes().collect::<Vec<_>>());
+    let mut reader = hasher.finalize_xof();
+
+    // update hash
+    let mut hashbuf = [0_u8; 32];
+    reader.read(&mut hashbuf);
+    hash.copy_from_slice(&hashbuf[..16]);
+
+    // generate challenges
+    let mut rng = ChaCha8Rng::from_seed(hashbuf);
+    challenges.resize_with(r, || PolyRingElem::challenge(&mut rng));
+
+    // compute z
+    let mut z = PolyVec::zero(dim);
+    tmp_wit
+        .vectors
+        .iter()
+        .zip(challenges.iter())
+        .for_each(|(w, c)| z.add_mul_assign(c, w));
+
+    // decompose z
+    let mut z = z.decomp(*z_base);
+
+    // compute output_wit.norm_square[i]
+    //         output_stat.norm_bound[i]
+    for i in 0..=z_length {
+        output_wit.norm_square[i] = output_wit.vectors[i].norm_square();
+        *squared_norm_bound += output_wit.norm_square[i];
+    }
+
+    proof.norm_square = *squared_norm_bound;
 }
